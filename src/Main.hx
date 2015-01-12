8ab8c36a7287eeb9bae6e9d9941c07852be3113f
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-01-11 18:53:14 -0700

    Move the spinning green square with your keyboard
    That totally counts as a game, right?

--- src/Main.hx
+++ src/Main.hx
@@ -5,7 +5,7 @@ import luxe.Vector;
 
 class Main extends luxe.Game {
   var player : Sprite;
-  var speed : Float = 0;
+  var speed : Float = 128;
 
   override function ready() {
     // do something here when assets have finished loading...
@@ -15,11 +15,28 @@ class Main extends luxe.Game {
       color: new Color().rgb(0xbada55),
       size: new Vector(128, 128)
     });
+
+    connectInput();
+  }
+
+  function connectInput() {
+    // wire up the keyboard
+    Luxe.input.bind_key('left', Key.left);
+    Luxe.input.bind_key('left', Key.key_a);
+
+    Luxe.input.bind_key('right', Key.right);
+    Luxe.input.bind_key('right', Key.key_d);
   }
 
   override function update(delta:Float) {
     // the amazingness
     player.rotation_z += 40 * delta;
+
+    if (Luxe.input.inputdown('left')) {
+      player.pos.x -= speed * delta;
+    } else if (Luxe.input.inputdown('right')) {
+      player.pos.x += speed * delta;
+    }
   }
 
 }
