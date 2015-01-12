48ca15c10ae57057fea9d51ca0bd0fc08721b1dd
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-01-12 07:30:34 -0700

    Draw a simple map

--- src/Main.hx
+++ src/Main.hx
@@ -6,17 +6,52 @@ import luxe.Vector;
 class Main extends luxe.Game {
   var player : Sprite;
   var speed : Float = 128;
+  var tileWidth = 32;
 
   override function ready() {
+    var map = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ];
+
     // do something here when assets have finished loading...
     player = new Sprite({
       name: 'the player',
-      pos: Luxe.screen.mid,
+      pos: new Vector(0 + tileWidth / 2, (10 * tileWidth) + tileWidth / 2),
       color: new Color().rgb(0xbada55),
-      size: new Vector(128, 128)
+      size: new Vector(tileWidth, tileWidth),
+      depth: 2
     });
 
+    drawMap(map);
     connectInput();
+  }
+
+  function drawMap(map : Array<Array<Int>>) {
+    for (row in 0...map.length) {
+      for (cell in 0...map[row].length) {
+        new Sprite({
+          centered: false,
+          pos: new Vector(cell * tileWidth, row * tileWidth),
+          color: (map[row][cell] == 1) ? new Color().rgb(0x0) : new Color().rgb(0xffffff),
+          size: new Vector(tileWidth, tileWidth)
+        });
+      }
+    }
   }
 
   function connectInput() {
@@ -30,8 +65,6 @@ class Main extends luxe.Game {
 
   override function update(delta:Float) {
     // the amazingness
-    player.rotation_z += 40 * delta;
-
     if (Luxe.input.inputdown('left')) {
       player.pos.x -= speed * delta;
     } else if (Luxe.input.inputdown('right')) {
