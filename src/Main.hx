60e95ac7f3a328c0022260ae623dfa6c57c66cf3
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-01-16 16:04:18 -0700

    Player handles its own collision logic

--- src/Main.hx
+++ src/Main.hx
@@ -5,7 +5,6 @@ import luxe.Vector;
 class Main extends luxe.Game {
   public var world : World;
   var player : Player;
-  var speed : Float = 128;
   var tileSize = 32;
   var playerSize = 24;
 
@@ -38,65 +37,23 @@ class Main extends luxe.Game {
     Luxe.input.bind_key('down', Key.key_s);
   }
 
-  override function update(delta:Float) {
+  override function update(delta : Float) {
     // Figure out our attempted keyboard movement, but ask our collision
     // functions to give us the real movement, based on map limits
-    var movement = new Vector(0, 0);
-
     if (Luxe.input.inputdown('left')) {
-      player.rendering.pos.x = limitLeftCollision(player.rendering.pos.x - (speed * delta));
+      player.moveX(-1 * player.maxSpeed * delta, world);
     }
 
     if (Luxe.input.inputdown('right')) {
-      player.rendering.pos.x = limitRightCollision(player.rendering.pos.x + (speed * delta));
+      player.moveX(player.maxSpeed * delta, world);
     }
 
     if (Luxe.input.inputdown('up')) {
-      player.rendering.pos.y = limitTopCollision(player.rendering.pos.y - (speed * delta));
+      player.moveY(-1 * player.maxSpeed * delta, world);
     }
 
     if (Luxe.input.inputdown('down')) {
-      player.rendering.pos.y = limitBottomCollision(player.rendering.pos.y + (speed * delta));
+      player.moveY(player.maxSpeed * delta, world);
     }
-  }
-
-  function limitBottomCollision(targetY : Float) : Float {
-    // check both bottom corners for collisions
-    if (mapTileIsSolid(player.rendering.pos.x, targetY + (playerSize - 1)) ||
-        mapTileIsSolid(player.rendering.pos.x + (playerSize - 1), targetY + (playerSize - 1))) {
-      targetY = Math.floor(targetY / tileSize) * tileSize + (tileSize - playerSize);
-    }
-    return targetY;
-  }
-
-  function limitTopCollision(targetY : Float) : Float {
-    if (mapTileIsSolid(player.rendering.pos.x, targetY) ||
-        mapTileIsSolid(player.rendering.pos.x + (playerSize - 1), targetY)) {
-      targetY = Math.ceil(targetY / tileSize) * tileSize;
-    }
-    return targetY;
-  }
-
-  function limitLeftCollision(targetX : Float) : Float {
-    if (mapTileIsSolid(targetX, player.rendering.pos.y) ||
-        mapTileIsSolid(targetX, player.rendering.pos.y + (playerSize - 1))) {
-      targetX = Math.ceil(targetX / tileSize) * tileSize;
-    }
-    return targetX;
-  }
-
-  function limitRightCollision(targetX : Float) : Float {
-    if (mapTileIsSolid(targetX + (playerSize - 1), player.rendering.pos.y) ||
-        mapTileIsSolid(targetX + (playerSize - 1), player.rendering.pos.y + (playerSize - 1))) {
-      targetX = Math.floor(targetX / tileSize) * tileSize + (tileSize - playerSize);
-    }
-    return targetX;
-  }
-
-  function mapTileIsSolid(x : Float, y : Float) : Bool {
-    return switch world.getValueAtTile(x, y) {
-      case MazeCell.wall: true;
-      case _: false;
-    };
   }
 }
--- src/Player.hx
+++ src/Player.hx
@@ -6,6 +6,7 @@ class Player {
   public var rendering : Sprite;
   public var isOnGround : Bool;
   public var velocity : Vector;
+  public var maxSpeed : Float = 128;
   public var size : Int;
 
   public function new(startPos : Vector, ?size = 24) {
@@ -20,5 +21,59 @@ class Player {
       size: new Vector(size, size),
       depth: 2
     });
+  }
+
+  public function moveX(howFar : Float, world : World) {
+    if (howFar > 0) {
+      rendering.pos.x = avoidRightCollision(rendering.pos.x + howFar, world);
+    } else {
+      rendering.pos.x = avoidLeftCollision(rendering.pos.x + howFar, world);
+    }
+  }
+  public function moveY(howFar : Float, world : World) {
+    if (howFar > 0) {
+      rendering.pos.y = avoidBottomCollision(rendering.pos.y + howFar, world);
+    } else {
+      rendering.pos.y = avoidTopCollision(rendering.pos.y + howFar, world);
+    }
+  }
+
+  function avoidLeftCollision(targetX : Float, world : World) : Float {
+    if (mapTileIsSolid(world, targetX, rendering.pos.y) ||
+        mapTileIsSolid(world, targetX, rendering.pos.y + (size - 1))) {
+      targetX = Math.ceil(targetX / world.tileSize) * world.tileSize;
+    }
+    return targetX;
+  }
+
+  function avoidRightCollision(targetX : Float, world : World) : Float {
+    if (mapTileIsSolid(world, targetX + (size -1), rendering.pos.y) ||
+        mapTileIsSolid(world, targetX + (size - 1), rendering.pos.y + (size - 1))) {
+      targetX = Math.floor(targetX / world.tileSize) * world.tileSize + (world.tileSize - size);
+    }
+    return targetX;
+  }
+
+  function avoidBottomCollision(targetY : Float, world : World) : Float {
+    if (mapTileIsSolid(world, rendering.pos.x, targetY + (size - 1)) ||
+        mapTileIsSolid(world, rendering.pos.x + (size - 1), targetY + (size - 1))) {
+      targetY = Math.floor(targetY / world.tileSize) * world.tileSize + (world.tileSize - size);
+    }
+    return targetY;
+  }
+
+  function avoidTopCollision(targetY : Float, world : World) : Float {
+    if (mapTileIsSolid(world, rendering.pos.x, targetY) ||
+        mapTileIsSolid(world, rendering.pos.x + (size - 1), targetY)) {
+      targetY = Math.ceil(targetY / world.tileSize) * world.tileSize;
+    }
+    return targetY;
+  }
+
+  function mapTileIsSolid(world : World, x : Float, y : Float) : Bool {
+    return switch world.getValueAtTile(x, y) {
+      case MazeCell.wall: true;
+      case _: false;
+    };
   }
 }
